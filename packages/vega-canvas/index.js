e037d0a3fe8d17e0ca2d505812aaa14af6f3b3d0
Dan Krusi <dan@nerves.ch>
2020-10-20 13:17:18 +0200

    strict validation of node-canvas package on NodeJS

--- packages/vega-canvas/index.js
+++ packages/vega-canvas/index.js
@@ -5,9 +5,13 @@ export {domCanvas} from './src/domCanvas';
 export {nodeCanvas} from './src/nodeCanvas';
 
 export function canvas(w, h, type) {
-  return domCanvas(w, h) || nodeCanvas(w, h, type) || null;
+  var ret = domCanvas(w, h) || nodeCanvas(w, h, type) || null;
+  if(ret != null) return ret;
+  else throw "Could not get a canvas to draw onto. Are you on NodeJS? If so make sure the 'canvas' package is installed and accessible (npm install canvas)!";
 }
 
 export function image() {
-  return domImage() || nodeImage() || null;
+  var ret = domImage() || nodeImage() || null;
+  if(ret != null) return ret;
+  else throw "Could not get a image to draw onto. Are you on NodeJS? If so make sure the 'canvas' package is installed and accessible (npm install canvas)!";
 }
